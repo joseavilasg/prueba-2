862ffc32eeccee6de46a26cc9e8f2a47328d0530
joseavilasg <joseavilasg.dev@gmail.com>
2024-04-24 10:39:07 -0500

    Resolve test

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-'use strict'
-const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
+"use strict";
+document.body.onload = addTodos;
+function addTodos() {
+    const to_do = [
+        "Alimentar al perro",
+        "Estudiar para prueba escrita",
+        "Pagar taxes",
+        "Pasear a perro",
+        "Comprar entradas para el cine",
+    ];
+
+    const GI = {
+        createElement(tag, atributes) {
+            let element = document;
+            return document.createElement(tag);
+        },
+    };
+
+    const todoHelper = document.getElementById("todo-helper");
+    const todoContainer = document.getElementById("todo-container");
+    const todoUl = document.createElement("ul");
+
+    to_do.forEach((todo) => {
+        const el = document.createElement("li");
+        el.createTextNode(todo);
+        const isChecked = el.getAttribute("checked");
+        if (isChecked) {
+            el.style["text-decoration"] = "line-through";
+            el.style["color"] = "gray";
+        }
+        todoUl.appendChild(el);
+    });
+
+    todoContainer.appendChild(todoUl);
+
+    todoContainer.insertBefore(todoContainer, todoHelper);
 }
-
-
-const to_do = [
-    'Alimentar al perro',
-    'Estudiar para prueba escrita',
-    'Pagar taxes',
-    'Pasear a perro',
-    'Comprar entradas para el cine',
-]
-
